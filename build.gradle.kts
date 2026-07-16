052c606d328ecd2146842f87f4c261a223beb03a
Zain Khan Swati <zainkhanswati54@gmail.com>
2026-07-16 07:26:15 +0500

    yes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,5 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("org.jetbrains.kotlin.jvm") version "1.9.24" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
